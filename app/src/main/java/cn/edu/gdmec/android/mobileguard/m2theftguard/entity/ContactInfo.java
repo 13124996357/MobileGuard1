d90d46fc99c15b39733a222869b0f8824725018e
13124996357 <2501867629@qq.com>
2017-10-22 05:36:32 +0800

    1

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/entity/ContactInfo.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/entity/ContactInfo.java
@@ -1,8 +1,9 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard.entity;
 
-/**
- * Created by Administrator on 2017/10/16 0016.
- */
+
 
 public class ContactInfo {
+    public String id;
+    public String name;
+    public String phone;
 }
